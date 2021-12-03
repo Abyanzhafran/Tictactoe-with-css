ded0de22649faac0757f53a6c4761be80d0ce305
Abyanzhafran <zhafranjupleks5758@gmail.com>
2021-12-03 11:12:53 +0700

    feat: add check win logic

--- public/js/turn.js
+++ public/js/turn.js
@@ -1,7 +1,7 @@
 var board;
 const player1 = 'O';
 const player2 = 'X';
-const winChecker = [
+const combos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -14,8 +14,6 @@ const winChecker = [
 
 const cells = document.querySelectorAll('.cell')
 startIt()
-
-// console.log(cells)
 
 function startIt() {
   board = Array.from(Array(9).keys())
@@ -30,7 +28,27 @@ function ifClick(square) {
   if (typeof board[square.target.id] == 'number') {
     board[square.target.id] = player1
     document.getElementById(square.target.id).innerText = player1
+    secVer(board)
+  }
+  // console.log(board)
+  // console.log(secVer(board))
+}
+
+function secVer(arr) {
+  ifClick
+  for (let i = 0; i < combos.length; i++) {
+    let checkVal = combos[i].filter(x => !arr.includes(x))
+    if (checkVal.length > 2) {
+      return alert('you winn boyy')
+    }
   }
 }
 
-
+// function checkIfWin() {
+//   for (let i = 0; i < combos.length; i++) {
+//     let checkVal = combos[i].filter(x => arr.includes(x))
+//     if (checkVal.length == arr.length) {
+//       return alert("you win boyy")
+//     }
+//   }
+// }
